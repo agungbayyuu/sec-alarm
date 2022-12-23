5ad1918ef5d78ac1478e80b33cf6db23c39057b0
agungbayyuu <agungbayyuu@gmail.com>
2022-12-24 00:50:17 +0700

    button back layout help and about

--- app/src/main/java/com/example/uas_ppb2_agung/about.java
+++ app/src/main/java/com/example/uas_ppb2_agung/about.java
@@ -3,12 +3,24 @@ package com.example.uas_ppb2_agung;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.Toolbar;
 
 public class about extends Activity {
+    ImageView back;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.layout_about);
+        back = findViewById(R.id.about_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(about.this, MainActivity.class);
+                startActivity(a);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/uas_ppb2_agung/help.java
+++ app/src/main/java/com/example/uas_ppb2_agung/help.java
@@ -1,13 +1,26 @@
 package com.example.uas_ppb2_agung;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.Toolbar;
 
 public class help extends Activity {
+    ImageView toolbar_about;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.layout_help);
+        toolbar_about = findViewById(R.id.help_back);
+        toolbar_about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(help.this, MainActivity.class);
+                startActivity(a);
+            }
+        });
     }
 }
